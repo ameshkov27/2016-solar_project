b5470ba89f30fe09068b84d8f59b2b1a1989c340
ameshkov <meshckowalex@gmail.com>
2020-03-28 15:47:29 +0300

    FIXED solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -47,8 +47,16 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    line = line.split()
+    star.type = line[0].lower()
+    star.R = line[1]
+    star.color = line[2]
+    star.m = line[3]
+    star.x = line[4]
+    star.y = line[5]
+    star.Vx = line[6]
+    star.Vy = line[7]
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -65,7 +73,15 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    line = line.split()
+    planet.type = line[0].lower()
+    planet.R = line[1]
+    planet.color = line[2]
+    planet.m = line[3]
+    planet.x = line[4]
+    planet.y = line[5]
+    planet.Vx = line[6]
+    planet.Vy = line[7]
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -81,8 +97,9 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            out_file.write(space_objects.type, space_objects.R, space_objects.color, space_objects.m, space_objects.x,
+                           space_objects.y, space_objects.Vx, space_objects.Vy)
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
